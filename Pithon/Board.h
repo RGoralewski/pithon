d1c04593a0acb53d7b4ac33c4d550e398d565242
RGoralewski <radekgoralewski@gmail.com>
2019-08-04 12:59:33 +0200

    Create Board class
    Board class is created. It contains a snake (Snake class) and a vector
    of bricks (Brick class). Board has implemented two game modes mechanism.
    In an easier mode player collect pi decimal expansion number one by one
    and in harder (pi knower mode) player see three different bricks and he
    has to choose the right one.

---
+++ Pithon/Board.h
@@ -0,0 +1,13 @@
+//
+//  Board.h
+//  Pithon
+//
+//  Created by Radek on 30/07/2019.
+//  Copyright © 2019 Radek. All rights reserved.
+//
+
+#ifndef Board_h
+#define Board_h
+
+
+#endif /* Board_h */
